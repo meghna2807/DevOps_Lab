907d753a6c09c063c92b9504184107508089df13
meghna2807 <meghnachauhan2807@gmail.com>
2026-02-09 15:38:23 +0530

    Initial commit

--- demo.py
+++ demo.py
@@ -3,5 +3,8 @@ import os
 os.system("git init")
 os.system("git add .")
 os.system('git commit -m "Initial commit"')
+os.system("git branch -M main")
+os.system("git remote add origin https://github.com/meghna2807/DevOps_Lab.git")
+os.system("git push -u origin main")
 
-print("Git repository initialized and commit done successfully")
+print("Source code pushed to GitHub successfully")
